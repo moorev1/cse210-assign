65523353db3bcba6a24e603785ba60c870d2daa3
moorev1 <vincentvangeemoore@yahoo.com>
2023-09-19 22:50:29 +0000

    added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompt the user for their first name
+        // It ugent
+        Console.Write("What is your first name: ");
+        string firstName = Console.ReadLine();
+
+        // Prompt the user for their last name
+        Console.Write("What is your last name: ");
+        string lastName = Console.ReadLine();
+
+        // Display the name in the specified format
+        Console.WriteLine("Your name is {0}, {1}, {0}", lastName, firstName);
+
+        // Pause to keep the console window open
+        Console.ReadLine();
     }
 }
