a6e9320436c3ca1fd9ac6a50f81bd0af35a59d12
moorev1 <vincentvangeemoore@yahoo.com>
2023-09-27 23:26:12 +0000

    added message

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,23 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();      // 1/1
+        Fraction fraction2 = new Fraction(5);     // 5/1
+        Fraction fraction3 = new Fraction(3, 4);  // 3/4
+        Fraction fraction4 = new Fraction(1, 3);  // 1/3
+
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
