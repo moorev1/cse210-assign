2410e9a118b562c62b490abdd1139f2a37bf367d
moorev1 <vincentvangeemoore@yahoo.com>
2023-10-10 22:40:53 +0000

    added message

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,21 @@
+// Program.cs
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 3));
+        shapes.Add(new Rectangle("Blue", 4, 5));
+        shapes.Add(new Circle("Green", 6));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}, Area: {shape.GetArea()}");
+        }
     }
 }
