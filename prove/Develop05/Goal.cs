1578d0e010e371f7eddf21f063436bcbcdbf8473
moorev1 <vincentvangeemoore@yahoo.com>
2023-10-12 23:36:46 +0000

    added message

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,18 @@
+public class Goal
+{
+    public string Name { get; set; }
+    public int Value { get; set; }
+    public bool Completed { get; set; }
+
+    public Goal(string name, int value)
+    {
+        Name = name;
+        Value = value;
+        Completed = false;
+    }
+
+    public virtual int RecordProgress()
+    {
+        return 0;
+    }
+}
